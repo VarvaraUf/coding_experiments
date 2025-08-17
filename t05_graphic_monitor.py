45448af366ba9f5ebe05b81edbca213267eb76c7
Varvara Ufimtseva <varvara.ufi@gmail.com>
2025-08-17 11:43:17 -0400

    t05_graphic_monitor.py: improvements

--- t05_graphic_monitor.py
+++ t05_graphic_monitor.py
@@ -1,19 +1,18 @@
 from machine import Pin,ADC,PWM
-from utime import sleep
-#define libraries
+import utime 
+
 
 led=PWM(Pin(7))
 pot=ADC(Pin(26,Pin.IN))
-#define the value we get from the led and pot.
 
 led.freq(1000)
 
-def  meet_the_light_bulp():
-    while True:#while loop
-        led.duty_u16(int((pot.read_u16())))
-        print(str(int((pot.read_u16()))))
-        #Turn on duty_u16the LED according to the value from the potentiometer.
-        sleep(0.1)#delay
+def meet_the_light_bulp():
+    while True:
+        led.duty_u16(pot.read_u16())
+        print("potantiometer",pot.read_u16())
+        
+        utime.sleep(0.3)
     
 meet_the_light_bulp()
     
