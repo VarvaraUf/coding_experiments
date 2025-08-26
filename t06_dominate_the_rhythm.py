9f6ca5bf53d24c15c72628e3e362bc72ab071539
Varvara Ufimtseva <varvara.ufi@gmail.com>
2025-08-26 11:55:49 -0400

    t06_dominate_the_rhythm.py: play_the_song improved

--- t06_dominate_the_rhythm.py
+++ t06_dominate_the_rhythm.py
@@ -1,24 +1,18 @@
-from machine import Pin,PWM,ADC,I2C #to acces the hardware picobricks
-from utime import sleep #time library
+from machine import Pin, PWM, ADC, I2C
 from picobricks import SSD1306_I2C
 import utime
 
 WIDTH=128
 HEIGHT=64
-#define the weight and height picobricks
 
-sda=Pin(4)
-scl=Pin(5)
-#we define sda and scl pins for inter-path communication
-i2c=I2C(0, sda=sda, scl=scl)#determine the frequency values
-oled=SSD1306_I2C(WIDTH, HEIGHT, i2c)
+i2c = I2C(0, sda=Pin(4), scl=Pin(5))
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
+button = Pin(10, Pin.IN, Pin.PULL_DOWN)
+pot = ADC(Pin(26))
+buzzer = PWM(Pin(20))
 
-button= Pin(10,Pin.IN,Pin.PULL_DOWN)
-pot=ADC(Pin(26))
-buzzer= PWM(Pin(20))
-#determine our input and output pins
-pressed = False
-rithm = 0
+button_clicked = False
+rhythm_speed = 0
 
 tones = {
 "A3": 220,
@@ -26,46 +20,64 @@ tones = {
 "E4": 330,
 "F4": 349
 }
-#define the tones
 
-mysong = ["A3","E4","E4","E4","E4","E4","E4","F4","E4","D4","F4","E4"]#let's define the tones required for our song in the correct order into a sequence
-noteTime = [1,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1]#define wait times between tones into an array
+mysong = ["A3", "E4", "E4", "E4", "E4", "E4", "E4", "F4", "E4", "D4", "F4", "E4"]
+noteTime = [1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1]
+melody = [("A3", 1), ("E4", 0.5), ("E4",0.5), ("E4",0.5),("E4",0.5), ("E4",0.5), ("E4",0.5), ("F4",0.5), ("E4",0.5), ("D4",0.5), ("F4",0.5), ("E4",1)]
+# for item in melod0y:
+#     note = item[0]
+#     time = item[1]
+#     print('note = ', note, "time = ", time)
+for note, time in melody:
+    print('note = ', note, "time = ", time)
 
-        
 def playtone(frequency):
     buzzer.duty_u16(6000)
     buzzer.freq(frequency)
-#define the frequencies of the buzzer
+
 def playsong(pin):
-    global pressed
-    pressed = True
-#play the tones with the right cooldowns
-#An finally we need to tell the pins when to trigger, and the function to call when they detect an event:       
-button.irq(trigger=Pin.IRQ_RISING, handler=playsong)
-note_count = 9999
-played_time = 0
-while True:
-    current_time = utime.ticks_ms()
-    oled.show()
-    oled.text("Press the button",0,0)
+    global button_clicked
+    button_clicked = True
+
+def play_the_song():
+    global button_clicked
+    button.irq(trigger=Pin.IRQ_RISING, handler=playsong)
+
+    note_count = 9999
+    note_play_start_time = 0
+    is_routine_enabled = False
+    while True:
+        current_time = utime.ticks_ms()
+        oled.show()
+        oled.text("Press the button",0,0)
+
     
-    if (note_count < len(mysong)):
-        oled.fill(0)
-        oled.text("Dominate ",30,10)
-        oled.text("the ",45,25)
-        oled.text("Rhythm ",35,40)
-        rithm=((pot.read_u16()/65535.0)*20) +1
-        if (current_time - played_time)/1000.0 >= noteTime[note_count]/rithm:
-            played_time = utime.ticks_ms()
-            playtone(tones[mysong[note_count]])
-            note_count += 1
-    else:
-        buzzer.duty_u16(0)
+        if is_routine_enabled and (note_count < len(melody)):
+            oled.fill(0)
+            oled.text("Dominate ", 30, 10)
+            oled.text("the ", 45, 25)
+            oled.text("Rhythm ", 35, 40)
+            rhythm_speed=((pot.read_u16()/65535.0)*20) + 1 # range from 1 to 20
+            if (current_time - note_play_start_time)/1000.0 >= melody[note_count][1]/rhythm_speed:
+                note_play_start_time = utime.ticks_ms()
+            # print("note_count = ", note_count
+            #       , "mysong[note_count] = ", mysong[note_count]
+            #       , "tones[mysong[note_count]]", tones[mysong[note_count]])
+                playtone(tones[melody[note_count][0]])
+                note_count += 1
+                if note_count == len(melody):
+                    note_count = 0
+                
+
+        if is_routine_enabled == False or note_count >= len(melody):
+            buzzer.duty_u16(0)
         
-    if pressed:
-        
-        note_count = 0
-        pressed = False
+        if button_clicked:        
+            note_count = 0
+            button_clicked = False
+            is_routine_enabled = not is_routine_enabled
+            
+play_the_song()
         
         
         
