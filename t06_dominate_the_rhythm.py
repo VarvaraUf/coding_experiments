e2ab346bf7899c587abc2655feb99f21abbaf28a
Varvara Ufimtseva <varvara.ufi@gmail.com>
2025-08-26 12:59:50 -0400

    t06_dominate_the_rhythm.py: play_the_song completed

--- t06_dominate_the_rhythm.py
+++ t06_dominate_the_rhythm.py
@@ -11,48 +11,44 @@ button = Pin(10, Pin.IN, Pin.PULL_DOWN)
 pot = ADC(Pin(26))
 buzzer = PWM(Pin(20))
 
-button_clicked = False
+is_routine_enabled = False
 rhythm_speed = 0
 
 tones = {
-"A3": 220,
-"D4": 294,
-"E4": 330,
-"F4": 349
+    "A3": 220,
+    "D4": 294,
+    "E4": 330,
+    "F4": 349
 }
 
-mysong = ["A3", "E4", "E4", "E4", "E4", "E4", "E4", "F4", "E4", "D4", "F4", "E4"]
-noteTime = [1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1]
 melody = [("A3", 1), ("E4", 0.5), ("E4",0.5), ("E4",0.5),("E4",0.5), ("E4",0.5), ("E4",0.5), ("F4",0.5), ("E4",0.5), ("D4",0.5), ("F4",0.5), ("E4",1)]
 # for item in melod0y:
 #     note = item[0]
 #     time = item[1]
 #     print('note = ', note, "time = ", time)
-for note, time in melody:
-    print('note = ', note, "time = ", time)
+# for note, time in melody:
+#     print('note = ', note, "time = ", time)
 
 def playtone(frequency):
     buzzer.duty_u16(6000)
     buzzer.freq(frequency)
 
-def playsong(pin):
-    global button_clicked
-    button_clicked = True
+def enable_buzzer(pin):
+    global is_routine_enabled
+    is_routine_enabled = not is_routine_enabled
 
 def play_the_song():
     global button_clicked
-    button.irq(trigger=Pin.IRQ_RISING, handler=playsong)
+    button.irq(trigger=Pin.IRQ_RISING, handler=enable_buzzer)
 
-    note_count = 9999
+    note_count = 0
     note_play_start_time = 0
-    is_routine_enabled = False
     while True:
         current_time = utime.ticks_ms()
         oled.show()
         oled.text("Press the button",0,0)
-
     
-        if is_routine_enabled and (note_count < len(melody)):
+        if is_routine_enabled:
             oled.fill(0)
             oled.text("Dominate ", 30, 10)
             oled.text("the ", 45, 25)
@@ -66,16 +62,10 @@ def play_the_song():
                 playtone(tones[melody[note_count][0]])
                 note_count += 1
                 if note_count == len(melody):
-                    note_count = 0
-                
+                    note_count = 0                
 
-        if is_routine_enabled == False or note_count >= len(melody):
+        if is_routine_enabled == False:
             buzzer.duty_u16(0)
-        
-        if button_clicked:        
-            note_count = 0
-            button_clicked = False
-            is_routine_enabled = not is_routine_enabled
             
 play_the_song()
         
