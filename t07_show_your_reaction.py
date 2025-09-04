691ca7f012513d0e3dbffd0178e64d9c6775c4a6
Varvara Ufimtseva <varvara.ufi@gmail.com>
2025-09-04 17:41:26 -0400

    t07_show_your_reaction.py show_your_reaction_score v0

--- t07_show_your_reaction.py
+++ t07_show_your_reaction.py
@@ -13,31 +13,66 @@ oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 button = Pin(10,Pin.IN,Pin.PULL_DOWN)
 led = Pin(7,Pin.OUT)
 #define our input and output pins
-while True:
-    led.value(0)
-    oled.fill(0)
-    oled.text("press the button",0,10)
-    oled.text("TO START!",25,25)
-    oled.show()
-    #print "Press the button" and "TO START!" on the OLED screen
-    while button.value()==0:
-        pass
-    oled.fill(0)
-    oled.text("Wait For LED",15,30)
-    oled.show()
-    #write "wait for LED" on the screen when the button is pressed
-    utime.sleep(urandom.uniform(1,5))
-    led.value(1)
-    timer_start=utime.ticks_ms()
-    #wait for a rondom second and turn on the led
-    while button.value()==0:
-        pass
-    timer_reaction=utime.ticks_diff(utime.ticks_ms(), timer_start)
-    pressed=True
-    oled.fill(0)
-    oled.text("Your Time",25,25)
-    oled.text(str(timer_reaction),50,50)
-    oled.show()
-    led.value(0)
-    utime.sleep(1.5)
-    #print the score and "Your Time" to the screen when the button is pressed.
+def show_your_reaction():
+    while True:
+        led.value(0)
+        oled.fill(0)
+        oled.text("press the button",0,10)
+        oled.text("TO START!",25,25)
+        oled.show()
+        
+        while button.value()==0:
+            pass
+        oled.fill(0)
+        oled.text("Wait For LED",15,30)
+        oled.show()
+        
+        utime.sleep(urandom.uniform(1,5))
+        led.value(1)
+        timer_start=utime.ticks_ms()
+        
+        while button.value()==0:
+            pass
+        timer_reaction=utime.ticks_diff(utime.ticks_ms(), timer_start)
+        oled.fill(0)
+        oled.text("Your Time",25,25)
+        oled.text(str(timer_reaction),50,50)
+        oled.show()
+        led.value(0)
+        utime.sleep(1.5)
+        
+def show_your_reaction_score():
+    best_score = 9999
+    while True:
+        led.value(0)
+        oled.fill(0)
+        oled.text("press the button",0,10)
+        oled.text("TO START!",25,25)
+        oled.show()
+        
+        while button.value()==0:
+            pass
+        oled.fill(0)
+        oled.text("Wait For LED",15,30)
+        oled.show()
+        
+        utime.sleep(urandom.uniform(1,5))
+        led.value(1)
+        timer_start=utime.ticks_ms()
+        
+        while button.value()==0:
+            pass
+        timer_reaction=utime.ticks_diff(utime.ticks_ms(), timer_start)
+        if timer_reaction < best_score:
+            best_score = timer_reaction
+        oled.fill(0)
+        oled.text("Your Time",25,25)
+        oled.text(str(timer_reaction),26,35)
+        oled.text("best score",26,45)
+        oled.text(str(best_score),26,55)
+        oled.show()
+        led.value(0)
+        utime.sleep(2.5)
+
+# show_your_reaction()
+show_your_reaction_score()
