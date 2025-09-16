f9e65438c46ff4fde0086034f6e4926c34c4e7d2
Varvara Ufimtseva <varvara.ufi@gmail.com>
2025-09-16 11:16:21 -0400

    t10_know_your_colors.py

--- t10_know_your_colors.py
+++ t10_know_your_colors.py
@@ -11,7 +11,7 @@ HEIGHT = 64
 sda=Pin(4)
 scl=Pin(5)
 i2c=I2C(0,sda=sda, scl=scl)
-ws = WS2812(pin_num=6, num_leds=1, brightness=0.3)
+ws = WS2812(pin_num=6, num_leds=1, brightness=0.05)
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 button = Pin(10,Pin.IN,Pin.PULL_DOWN)
@@ -20,6 +20,9 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+COLORS = (RED,GREEN,BLUE,WHITE)
+TEXTS = ("RED","GREEN","BLUE","WHITE")
 
 oled.fill(0)
 oled.show()
@@ -30,46 +33,20 @@ ws.pixels_show()
 global button_pressed
 score=0
 button_pressed = False
+ledcolor = None
 
 def random_rgb():
     global ledcolor
-    ledcolor=int(urandom.uniform(1,4))
-    if ledcolor == 1:
-        ws.pixels_fill(RED)
-        ws.pixels_show()
-    elif ledcolor == 2:
-        ws.pixels_fill(GREEN)
-        ws.pixels_show()
-    elif ledcolor == 3:
-        ws.pixels_fill(BLUE)
-        ws.pixels_show()
-    elif ledcolor == 4:
-        ws.pixels_fill(WHITE)
-        ws.pixels_show()
+    ledcolor=urandom.randint(0,3)
+    ws.pixels_fill(COLORS[ledcolor])
+    ws.pixels_show()
 
 def random_text():
     global oledtext
-    oledtext=int(urandom.uniform(1,4))
-    if oledtext == 1:
-        oled.fill(0)
-        oled.show()
-        oled.text("RED",45,32)
-        oled.show()
-    elif oledtext == 2:
-        oled.fill(0)
-        oled.show()
-        oled.text("GREEN",45,32)
-        oled.show()
-    elif oledtext == 3:
-        oled.fill(0)
-        oled.show()
-        oled.text("BLUE",45,32)
-        oled.show()
-    elif oledtext == 4:
-        oled.fill(0)
-        oled.show()
-        oled.text("WHITE",45,32)
-        oled.show()
+    oledtext=urandom.randint(0,3)
+    oled.fill(0)
+    oled.text(TEXTS[oledtext],45,32)
+    oled.show()
 
 def button_reader_thread():
     while True:
@@ -97,13 +74,10 @@ for i in range(10):
     random_rgb()
     button_pressed=False
     utime.sleep(1.5)
-    oled.fill(0)
-    oled.show()
-    ws.pixels_fill(BLACK)
-    ws.pixels_show()
 utime.sleep(1.5)
 oled.fill(0)
-oled.show()
 oled.text("Your total score:",0,20)
 oled.text(str(score), 57,40)
 oled.show()
+ws.pixels_fill(BLACK)
+ws.pixels_show()
