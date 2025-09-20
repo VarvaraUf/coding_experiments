95f81a207f6b5081629cb75ca9d40e0e4b2a52f7
Varvara Ufimtseva <varvara.ufi@gmail.com>
2025-09-20 13:39:57 -0400

    t11_magic_lamp.py magic_lamp_rrq

--- t11_magic_lamp.py
+++ t11_magic_lamp.py
@@ -1,14 +1,36 @@
-from machine import Pin #to access the hardware on the pico
+from machine import Pin
 import utime
-sensor=Pin(9,Pin.IN) #initialize digital pin 1 as an INPUT for Sensor
-led=Pin(7,Pin.OUT)#initialize digital pin 7 as an OUTPUT for LED
-print('magic lamp 0')
-while True:
-    # print('magic lamp 1')
-    #When sensor value is '0', the relay will be '1'
-    # utime.sleep(0.05)
-    # print('magic lamp 2, sensor value = ', sensor.value())
-    if sensor.value() == 1:
-        led.value(1)  
-    else:
-        led.value(0)
+def magic_lamp():
+    sensor=Pin(9,Pin.IN)
+    led=Pin(7,Pin.OUT)
+
+    while True:
+        if sensor.value() == 1:
+            led.value(1)  
+        else:
+            led.value(0)
+
+
+def magic_lamp_rrq():
+    global last_clap_time
+    last_clap_time = 0
+
+    sound_sensor = Pin(9,Pin.IN)
+    led = Pin(7,Pin.OUT)
+    led.value(0)
+
+    def on_clap(pin):
+        global last_clap_time
+        
+        if (utime.ticks_ms() - last_clap_time) <= 200:
+            print(utime.ticks_ms(), 'on_clap skip', pin.value())
+            return
+        print(utime.ticks_ms(), 'on_clap toggle', pin.value())
+        led.value(not led.value())
+        last_clap_time = utime.ticks_ms()
+        
+    sound_sensor.irq(trigger=Pin.IRQ_RISING, handler=on_clap)
+    while True:
+        pass
+
+magic_lamp_rrq()
